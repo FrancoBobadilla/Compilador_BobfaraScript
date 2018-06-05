0451e212e7fbfbc97d2825fe3f1f7ed63766a8a2
NNelo <nelogino@gmail.com>
2018-06-05 17:24:20 -0300

    Se implementa interfaz de Analizador

--- Analizador.h
+++ Analizador.h
@@ -1,17 +1,55 @@
-//
-// Created by FrancoBobadilla on 5/6/2018.
-//
-
 #ifndef UNTITLED_ANALIZADOR_H
 #define UNTITLED_ANALIZADOR_H
 
-class Analizador {
+#define tipo bool
+
+class Analizador{
 private:
+		
+	FILE *A;
+	
+	tipo lexico();
+	
+	tipo sintactico();	
+	
+	// tipo semantico();
+	
+	
+	tipo Programa();
+	
+	tipo bloque();
+	
+	tipo otra_sentencia();
+	
+	tipo sentencia();
+	
+	tipo asignacion();
+	
+	tipo expresion();
+	
+	tipo mas_terminos();
+	
+	tipo termino();
+	
+	tipo mas_factores();
+	
+	tipo factor();
+	
+	tipo lectura();
+	
+	tipo escritura();
+	
+	tipo variable();
+	
+	tipo constante();
+	
+	tipo vacio();	
 
-public:
-    Analizador(char *);
-
-    bool analizar();
+public: 
+	Analizador(FILE *);
+	
+	tipo analizar();
+		
 };
 
-#endif //UNTITLED_ANALIZADOR_H
+#endif
